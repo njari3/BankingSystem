9f617941dcd0eaa3c16165ee4ba0c419d0b76fdf
nyari <nyari.t@hotmail.com>
2023-04-14 16:56:22 +0200

    add models

---
+++ BankingSystem/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingSystem.Models
+{
+    public class Account
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public decimal Balance { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ BankingSystem/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingSystem.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Account> Accounts { get; set; } = new List<Account>();
+    }
+}
